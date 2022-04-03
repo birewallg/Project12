d42e08582f64dba05d9fde98340dde50f333cedf
birewallg <g.sstrav@gmail.com>
2022-04-04 00:05:04 +0300

    minor fix models

--- src/main/java/local/uniclog/model/actions/Log.java
+++ src/main/java/local/uniclog/model/actions/Log.java
@@ -2,14 +2,15 @@ package local.uniclog.model.actions;
 
 import local.uniclog.model.ActionType;
 import local.uniclog.model.ActionsInterface;
+import lombok.Builder;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
+@Data
+@Builder
 public class Log implements ActionsInterface {
-
-    public Log(String description) {
-
-    }
+    private String description;
 
     @Override
     public void execute(String... args) {
