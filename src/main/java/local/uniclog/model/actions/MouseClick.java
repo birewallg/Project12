16eaeb90a8a2cbc8082b1966c34eb1a92e5553a6
birewallg <g.sstrav@gmail.com>
2022-04-04 01:49:33 +0300

    final MouseClick model

--- src/main/java/local/uniclog/model/actions/MouseClick.java
+++ src/main/java/local/uniclog/model/actions/MouseClick.java
@@ -4,11 +4,13 @@ import local.uniclog.model.ActionType;
 import local.uniclog.model.ActionsInterface;
 import lombok.Builder;
 import lombok.Data;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.awt.*;
+import java.util.concurrent.TimeUnit;
 
-import static java.awt.event.InputEvent.BUTTON1_DOWN_MASK;
+import static java.awt.event.InputEvent.*;
 
 @Slf4j
 @Data
@@ -17,36 +19,53 @@ public class MouseClick implements ActionsInterface {
     @Builder.Default
     private Point point = new Point(0, 0);
     @Builder.Default
-    private Integer action = 0;
+    private String action = "";
     @Builder.Default
     private Integer count = 0;
     @Builder.Default
-    private Integer period = 0;
+    private Long wait = 50L;
     @Builder.Default
-    private Integer sleepAfter = 0;
+    private Long sleepAfter = 0L;
 
     @Override
+    @SneakyThrows
     public void execute(String... args) {
+        switch (action) {
+            case "BUTTON1":
+                mouseAction(BUTTON1_DOWN_MASK);
+                break;
+
+            case "BUTTON2":
+                mouseAction(BUTTON2_DOWN_MASK);
+                break;
+
+            case "BUTTON3":
+                mouseAction(BUTTON3_DOWN_MASK);
+                break;
+
+            default:
+                log.info("MouseClick: {}", getType());
+                break;
+        }
+        log.info("MouseClick: {}", this);
+    }
+
+    private void mouseAction(int buttonCode) {
         try {
-            switch (action) {
-                case 1:
-                    Robot robot = getRobot();
-                    robot.mouseMove(point.x, point.y);
-                    robot.mousePress(BUTTON1_DOWN_MASK);
-                    robot.mouseRelease(BUTTON1_DOWN_MASK);
-                    break;
-
-                case 2:
-                    log.info("MouseClick: action not realise");
-                    break;
-
-                default:
-                    log.info("MouseClick: {}", getType());
-                    break;
+            Integer loopCount = 0;
+            while (!loopCount.equals(count)) {
+                Robot robot = getRobot();
+                robot.mouseMove(point.x, point.y);
+                robot.mousePress(buttonCode);
+                robot.mouseRelease(buttonCode);
+                TimeUnit.MILLISECONDS.sleep(wait);
+                loopCount++;
+                if (loopCount.equals(count))
+                    TimeUnit.MILLISECONDS.sleep(sleepAfter);
             }
-            log.info("MouseClick: {}", this);
-        } catch (AWTException e) {
+        } catch (InterruptedException | AWTException e) {
             log.info("MouseClick Error: {}, object {}", e.getMessage(), this);
+            Thread.currentThread().interrupt();
         }
     }
 
