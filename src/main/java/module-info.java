4e6b54d08e3ed387fa1a9b2b3abd178180f8689a
birewallg <g.sstrav@gmail.com>
2022-04-05 21:10:20 +0300

    config app with modules, add javafx support

---
+++ src/main/java/module-info.java
@@ -0,0 +1,13 @@
+module local.uniclog {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+    requires org.controlsfx.controls;
+    requires lombok;
+    requires org.slf4j;
+    requires java.desktop;
+    requires com.google.gson;
+
+    opens local.uniclog.ui to javafx.fxml;
+    exports local.uniclog;
+}
