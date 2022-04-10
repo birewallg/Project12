2e04bb87cfbe5b2bb103b8cdb05dfe540ce82e04
birewallg <g.sstrav@gmail.com>
2022-04-10 16:44:33 +0300

    add: enums string value

--- src/main/java/local/uniclog/model/ActionType.java
+++ src/main/java/local/uniclog/model/ActionType.java
@@ -1,19 +1,38 @@
 package local.uniclog.model;
 
+import java.util.Arrays;
+
 /**
  * click_l - (x, y, up/down/click, count, period, sleepAfter)
  * click_r - (x, y, up/down/click, count, period, sleepAfter)
  * sleep - (time)
  * log - (x, y, color, alert)
- *
+ * <p>
  * key - buttons ?
- *
+ * <p>
  * default - console log
  */
 public enum ActionType {
-    MOUSE_CLICK,
-    SLEEP,
-    LOG,
+    MOUSE_CLICK("MOUSE_CLICK"),
+    SLEEP("SLEEP"),
+    LOG("LOG"),
 
-    DEFAULT
+    DEFAULT("DEFAULT");
+
+    private final String value;
+
+    ActionType(String value) {
+        this.value = value;
+    }
+
+    public static ActionType getType(String msg) {
+        return Arrays.stream(ActionType.values())
+                .filter(it -> it.value.equalsIgnoreCase(msg))
+                .findFirst()
+                .orElse(DEFAULT);
+    }
+
+    public String getStringValue() {
+        return value;
+    }
 }
--- src/main/java/local/uniclog/model/MouseButtonType.java
+++ src/main/java/local/uniclog/model/MouseButtonType.java
@@ -1,7 +1,26 @@
 package local.uniclog.model;
 
+import java.util.Arrays;
+
 public enum MouseButtonType {
-    BUTTON1,
-    BUTTON2,
-    BUTTON3
+    BUTTON1("BUTTON1"),
+    BUTTON2("BUTTON2"),
+    BUTTON3("BUTTON3");
+
+    private final String value;
+
+    MouseButtonType(String value) {
+        this.value = value;
+    }
+
+    public static MouseButtonType getType(String msg) {
+        return Arrays.stream(MouseButtonType.values())
+                .filter(it -> it.value.equalsIgnoreCase(msg))
+                .findFirst()
+                .orElse(BUTTON1);
+    }
+
+    public String getStringValue() {
+        return value;
+    }
 }
