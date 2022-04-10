ac5c343c562ddd0b0cf53b06e4df7a0fd6562b88
birewallg <g.sstrav@gmail.com>
2022-04-10 16:43:39 +0300

    add: data utils service

---
+++ src/main/java/local/uniclog/services/DataUtils.java
@@ -0,0 +1,34 @@
+package local.uniclog.services;
+
+public class DataUtils {
+
+    /**
+     * Get Integer value from String
+     *
+     * @param value        string value
+     * @param defaultValue default
+     * @return Integer
+     */
+    public static Integer getInteger(String value, int defaultValue) {
+        try {
+            return Integer.parseInt(value);
+        } catch (NumberFormatException ignore) {
+            return defaultValue;
+        }
+    }
+
+    /**
+     * Get Long value from String
+     *
+     * @param value        string value
+     * @param defaultValue default
+     * @return Long
+     */
+    public static Long getLong(String value, long defaultValue) {
+        try {
+            return Long.parseLong(value);
+        } catch (NumberFormatException ignore) {
+            return defaultValue;
+        }
+    }
+}
