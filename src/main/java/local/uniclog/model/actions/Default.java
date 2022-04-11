6e17e9afa96d792009bc6b59d94303daba570354
birewallg <g.sstrav@gmail.com>
2022-04-11 23:56:21 +0300

    add: Default action

---
+++ src/main/java/local/uniclog/model/actions/Default.java
@@ -0,0 +1,19 @@
+package local.uniclog.model.actions;
+
+import local.uniclog.model.ActionsInterface;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.Map;
+
+@Slf4j
+public class Default implements ActionsInterface {
+    @Override
+    public void execute(String... args) {
+        log.debug("Default action");
+    }
+
+    @Override
+    public ActionsInterface fieldInjection(Map<String, String> args) {
+        return null;
+    }
+}
