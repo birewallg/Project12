13f25c90d6042bdbb39bce7a2f12abcefebbc79c
birewallg <g.sstrav@gmail.com>
2022-04-24 12:17:29 +0900

    fix: utility class

--- src/main/java/local/uniclog/utils/ConfigConstants.java
+++ src/main/java/local/uniclog/utils/ConfigConstants.java
@@ -1,5 +1,9 @@
 package local.uniclog.utils;
 
 public class ConfigConstants {
+    private ConfigConstants() {
+        throw new IllegalStateException("Utility class");
+    }
+
     public static final String DEFAULT_FILE_PATH = "c:/";
 }
