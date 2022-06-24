45e8f856417d277c6e25251908d495c15ef375f1
birewallg <g.sstrav@gmail.com>
2022-06-24 18:29:54 +0300

    refactor: ui control elements

--- src/main/java/local/uniclog/ui/controlls/SceneControlService.java
+++ src/main/java/local/uniclog/ui/controlls/SceneControlService.java
@@ -11,62 +11,92 @@ import java.awt.*;
 
 import static javafx.scene.Cursor.*;
 
+/**
+ * App control helper-service
+ * - resize app
+ * - move app
+ *
+ * @version 1.0
+ */
 @NoArgsConstructor
 @AllArgsConstructor
 public class SceneControlService {
 
     private final Point deltaMove = new Point(0, 0);
     private final Point deltaResize = new Point(0, 0);
-    private int border = 4;
+    private int borderSizePixel = 4;
 
+    /**
+     * Set temp values by mouse pressed
+     *
+     * @param mouseEvent mouse event
+     * @param root       root
+     * @param stage      stage
+     */
     public void setOnMousePressed(MouseEvent mouseEvent, Parent root, Stage stage) {
         root.requestFocus();
         deltaMove.setLocation(stage.getX() - mouseEvent.getScreenX(), stage.getY() - mouseEvent.getScreenY());
         deltaResize.setLocation(stage.getWidth() - mouseEvent.getSceneX(), stage.getHeight() - mouseEvent.getSceneY());
     }
 
+    /**
+     * Set cursor type by mouse move
+     *
+     * @param mouseEvent mouse event
+     * @param scene      scene
+     */
     public void setOnMouseMoved(MouseEvent mouseEvent, Scene scene) {
         var mouseEventX = mouseEvent.getSceneX();
         var mouseEventY = mouseEvent.getSceneY();
         var sceneWidth = scene.getWidth();
         var sceneHeight = scene.getHeight();
         var cursorEvent = DEFAULT;
-        if (mouseEventX < border && mouseEventY < border) {
+        if (mouseEventX < borderSizePixel && mouseEventY < borderSizePixel) {
             cursorEvent = NW_RESIZE;
-        } else if (mouseEventX < border && mouseEventY > sceneHeight - border) {
+        } else if (mouseEventX < borderSizePixel && mouseEventY > sceneHeight - borderSizePixel) {
             cursorEvent = SW_RESIZE;
-        } else if (mouseEventX > sceneWidth - border && mouseEventY < border) {
+        } else if (mouseEventX > sceneWidth - borderSizePixel && mouseEventY < borderSizePixel) {
             cursorEvent = NE_RESIZE;
-        } else if (mouseEventX > sceneWidth - border && mouseEventY > sceneHeight - border) {
+        } else if (mouseEventX > sceneWidth - borderSizePixel && mouseEventY > sceneHeight - borderSizePixel) {
             cursorEvent = SE_RESIZE;
-        } else if (mouseEventX < border) {
+        } else if (mouseEventX < borderSizePixel) {
             cursorEvent = W_RESIZE;
-        } else if (mouseEventX > sceneWidth - border) {
+        } else if (mouseEventX > sceneWidth - borderSizePixel) {
             cursorEvent = E_RESIZE;
-        } else if (mouseEventY < border) {
+        } else if (mouseEventY < borderSizePixel) {
             cursorEvent = N_RESIZE;
-        } else if (mouseEventY > sceneHeight - border) {
+        } else if (mouseEventY > sceneHeight - borderSizePixel) {
             cursorEvent = S_RESIZE;
         }
         scene.setCursor(cursorEvent);
     }
 
+    /**
+     * Set new stage position or stage resize
+     *
+     * @param mouseEvent mouse event
+     * @param scene      scene
+     * @param stage      stage
+     */
     public void setOnMouseDragged(MouseEvent mouseEvent, Scene scene, Stage stage) {
+        // region stage move
         if (scene.getCursor().equals(DEFAULT)) {
             stage.setX(mouseEvent.getScreenX() + deltaMove.getX());
             stage.setY(mouseEvent.getScreenY() + deltaMove.getY());
             return;
         }
+        // region horizontal stage resize
         if (!scene.getCursor().equals(W_RESIZE) && !scene.getCursor().equals(E_RESIZE)) {
             horizontalResize(mouseEvent, scene, stage);
         }
+        // region vertical stage resize
         if (!scene.getCursor().equals(N_RESIZE) && !scene.getCursor().equals(S_RESIZE)) {
             verticalResize(mouseEvent, scene, stage);
         }
     }
 
     private void horizontalResize(MouseEvent mouseEvent, Scene scene, Stage stage) {
-        var minHeight = stage.getMinHeight() > (border * 2) ? stage.getMinHeight() : (border * 2);
+        var minHeight = stage.getMinHeight() > (borderSizePixel * 2) ? stage.getMinHeight() : (borderSizePixel * 2);
         if (scene.getCursor().equals(NW_RESIZE) || scene.getCursor().equals(N_RESIZE) || scene.getCursor().equals(NE_RESIZE)) {
             if (stage.getHeight() > minHeight || deltaResize.getY() < 0) {
                 stage.setHeight(stage.getY() - mouseEvent.getScreenY() + stage.getHeight());
@@ -80,7 +110,7 @@ public class SceneControlService {
     }
 
     private void verticalResize(MouseEvent mouseEvent, Scene scene, Stage stage) {
-        var minWidth = stage.getMinWidth() > (border * 2) ? stage.getMinWidth() : (border * 2);
+        var minWidth = stage.getMinWidth() > (borderSizePixel * 2) ? stage.getMinWidth() : (borderSizePixel * 2);
         if (scene.getCursor().equals(NW_RESIZE) || scene.getCursor().equals(W_RESIZE) || scene.getCursor().equals(SW_RESIZE)) {
             if (stage.getWidth() > minWidth || deltaResize.getX() < 0) {
                 stage.setWidth(stage.getX() - mouseEvent.getScreenX() + stage.getWidth());
