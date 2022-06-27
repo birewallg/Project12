6b73785e5c8db862daf999dde49b4df873129702
birewallg <g.sstrav@gmail.com>
2022-06-27 18:33:06 +0300

    PROJECT-12-T-7: add save/load json format + tests
    добавлена возможность сохранять обьекты в json

--- src/main/java/local/uniclog/services/support/FileServiceWrapper.java
+++ src/main/java/local/uniclog/services/support/FileServiceWrapper.java
@@ -1,9 +1,11 @@
 package local.uniclog.services.support;
 
+import com.google.gson.Gson;
 import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
 
 import java.io.*;
+import java.lang.reflect.Type;
 import java.util.Scanner;
 
 import static local.uniclog.utils.ConfigConstants.TEMPLATE_UTILITY_CLASS;
@@ -49,6 +51,26 @@ public class FileServiceWrapper {
         try (var fis = new FileInputStream(path);
              var ois = new ObjectInputStream(fis)) {
             return objectType.cast(ois.readObject());
+        }
+    }
+
+    @SneakyThrows(IOException.class)
+    public static void saveJson(String path, Object object, Type type) {
+        try (var fw = new FileWriter(path)) {
+            fw.write(new Gson().toJson(object, type));
+            fw.flush();
+        }
+    }
+
+    @SneakyThrows(IOException.class)
+    public static <T> T loadJson(String path, Type objectType) {
+        try (var fr = new FileReader(path)) {
+            int c;
+            var stringBuilder = new StringBuilder();
+            while ((c = fr.read()) != -1) {
+                stringBuilder.append((char) c);
+            }
+            return new Gson().fromJson(stringBuilder.toString(), objectType);
         }
     }
 }
