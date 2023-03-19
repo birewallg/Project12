80565a99ad2be22d2f0c4b44cf60fa0345d5b8e9
birewallg <g.sstrav@gmail.com>
2023-03-19 04:52:47 +0300

    PROJECT-12-T-16: add constants

--- src/main/java/local/uniclog/utils/ConfigConstants.java
+++ src/main/java/local/uniclog/utils/ConfigConstants.java
@@ -2,15 +2,18 @@ package local.uniclog.utils;
 
 public class ConfigConstants {
 
+    public static final String TEMPLATE_UTILITY_CLASS = "Utility class";
     public static final String TOP_LOGO_TEXT = "early access ver.";
     public static final String DEFAULT_FILE_PATH = "c:/";
+    public static final String TEMPLATE_CONFIG_PATH = "config.ini";
     public static final String GUI_BUTTON_RED = "gui-button-red";
     public static final String GUI_BUTTON_GREEN = "gui-button-green";
-    public static final String TEMPLATE_UTILITY_CLASS = "Utility class";
     public static final String TEMPLATE_NOT_SET_CONTROLS = "Warn: not set controls";
-    public static final String TEMPLATE_CONFIG_PATH = "config.ini";
     public static final String TEMPLATE_NEW_MACROS = "New Macros";
     public static final String EMPTY = "";
+    public static final Character PARAM_START = '[';
+    public static final Character PARAM_END = ']';
+    public static final Character COMMA = ',';
 
     private ConfigConstants() {
         throw new IllegalStateException(TEMPLATE_UTILITY_CLASS);
