85e8031f83665b454ad2a4297d22b7b3418c8781
birewallg <g.sstrav@gmail.com>
2023-03-28 23:17:50 +0300

    PROJECT-12-T-24: save config by timer

--- src/main/java/local/uniclog/ui/model/MacrosItem.java
+++ src/main/java/local/uniclog/ui/model/MacrosItem.java
@@ -1,15 +1,15 @@
 package local.uniclog.ui.model;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import static local.uniclog.utils.ConfigConstants.EMPTY;
 import static local.uniclog.utils.ConfigConstants.TEMPLATE_NEW_MACROS;
 
-@Data
+@Setter
+@Getter
 @AllArgsConstructor
 @NoArgsConstructor
+@EqualsAndHashCode
 public class MacrosItem {
     private String name = TEMPLATE_NEW_MACROS;
 
@@ -17,6 +17,15 @@ public class MacrosItem {
 
     private String path = EMPTY;
 
+    private Integer activation = null;
+
+    public MacrosItem(String name, String text) {
+        this(name, text, EMPTY, null);
+    }
+
+    public MacrosItem(String name, String text, String path) {
+        this(name, text, path, null);
+    }
 
     @Override
     public String toString() {
