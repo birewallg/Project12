4b64d821c68612cb5656f2d22296fdb22c008a90
birewallg <g.sstrav@gmail.com>
2023-05-15 16:15:01 +0300

    PROJECT-12-T-16: fix save/load file tests

--- src/test/java/local/uniclog/services/support/FileServiceWrapperTest.java
+++ src/test/java/local/uniclog/services/support/FileServiceWrapperTest.java
@@ -1,25 +1,31 @@
 package local.uniclog.services.support;
 
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
 import java.util.Objects;
 import java.util.Set;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.*;
 
 class FileServiceWrapperTest {
     private static final String PATH_OF_CONFIG = "test.json";
 
+    @AfterEach
+    void afterEach() {
+        assertTrue(new File(PATH_OF_CONFIG).delete());
+    }
+
     @Test
     void saveLoadJsonTest() {
         var config = new TestData();
-        FileServiceWrapper.saveObjectAsJson(PATH_OF_CONFIG, config, TestData.class);
-        var assertConfig = FileServiceWrapper.loadObjectFromJson(PATH_OF_CONFIG, TestData.class);
-
-        assertEquals(assertConfig, config);
-        assertTrue(new File(PATH_OF_CONFIG).delete());
+        var savedConfig = FileServiceWrapper.saveObjectAsJson(PATH_OF_CONFIG, config, TestData.class);
+        var loadConfig = FileServiceWrapper.loadObjectFromJson(PATH_OF_CONFIG, TestData.class);
+        assertAll(
+                () -> assertEquals(loadConfig, savedConfig),
+                () -> assertEquals(savedConfig, config),
+                () -> assertEquals(loadConfig, config));
     }
 
     @Test
@@ -31,16 +37,14 @@ class FileServiceWrapperTest {
     }
 
     static class TestData {
-        int num = 1;
-        String text = "text";
-        Set<String> setOfStrings = Set.of("str1", "str2", "str3", "str4");
+        final int num = 1;
+        final String text = "text";
+        final Set<String> setOfStrings = Set.of("str1", "str2", "str3", "str4");
 
         @Override
         public boolean equals(final Object o) {
             if (o == this) return true;
             if (!(o instanceof final TestData other)) return false;
-            if (this.num != other.num) return false;
-            if (!Objects.equals(this.text, other.text)) return false;
             return Objects.equals(this.setOfStrings.toString(), other.setOfStrings.toString());
         }
     }
