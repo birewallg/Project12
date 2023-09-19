8d354a2bafab6bed4194f41db7f053625e0ebb54
birewallg <g.sstrav@gmail.com>
2023-09-19 23:57:52 +0300

    PROJECT-12-T-30, PROJECT-12-T-29: refactor DataUtils

---
+++ src/main/java/local/uniclog/utils/DataUtils.kt
@@ -0,0 +1,75 @@
+package local.uniclog.utils
+
+object DataUtils {
+
+    /**
+     * Get Integer value from String
+     *
+     * @param value        string value
+     * @param defaultValue default
+     * @return Integer
+     */
+    @JvmStatic
+    fun getInteger(value: String, defaultValue: Int): Int =
+        value.toIntOrNull() ?: defaultValue
+
+    /**
+     * Get Long value from String
+     * @param value        string value
+     * @param defaultValue default
+     * @return Long
+     */
+    @JvmStatic
+    fun getLong(value: String, defaultValue: Long): Long =
+        value.toLongOrNull() ?: defaultValue
+
+    /**
+     * Get Boolean value from String
+     *
+     * @param value        string value
+     * @param defaultValue default
+     * @return Long
+     */
+    @JvmStatic
+    fun getBoolean(value: String, defaultValue: Boolean): Boolean =
+        value.toBooleanStrictOrNull() ?: defaultValue
+
+    /**
+     * Convert Int code to ASCII code
+     */
+    @JvmStatic
+    fun convertCodeToASCII(code: Int): String? {
+        return when (code) {
+            in 48..57, in 65..90 -> Char(code).toString()
+            in codeNameMap -> codeNameMap[code]
+            else -> "{$code}"
+        }
+    }
+
+    /**
+     * Convert ASCII code to Int code
+     */
+    @JvmStatic
+    fun convertASCIIToCode(codeName: String): Int? {
+        return when {
+            codeNameMap.containsValue(codeName) -> codeNameMap.entries.firstOrNull { it.value == codeName }?.key
+            codeName.length == 1 -> codeName[0].code
+            else -> """(?<=\{).*(?=})""".toRegex().find(codeName)?.groupValues?.get(0)?.toIntOrNull()
+        }
+    }
+
+    private val codeNameMap = mapOf(
+        9 to "TUB",
+        20 to "CAPS_LOCK",
+        13 to "ENTER",
+        27 to "ESC",
+        32 to "SPACE",
+        160 to "R_SHIFT", 161 to "L_SHIFT",
+        162 to "R_CTRL", 163 to "L_CTRL",
+        164 to "L_ALT", 165 to "R_ALT",
+        192 to "~",
+        112 to "F1", 113 to "F2", 114 to "F3", 115 to "F4",
+        116 to "F5", 117 to "F6", 118 to "F7", 119 to "F8",
+        120 to "F9", 121 to "F10", 122 to "F11", 123 to "F12"
+    )
+}
--- src/test/java/local/uniclog/utils/DataUtilsTest.java
+++ src/test/java/local/uniclog/utils/DataUtilsTest.java
@@ -7,21 +7,42 @@ import static org.junit.jupiter.api.Assertions.*;
 class DataUtilsTest {
 
     @Test
-    void getInteger() {
+    void getIntegerTest() {
         assertEquals(5, DataUtils.getInteger("5", 0));
         assertEquals(0, DataUtils.getInteger("five", 0));
     }
 
     @Test
-    void getLong() {
+    void getLongTest() {
         assertEquals(5L, DataUtils.getLong("5", 0L));
         assertEquals(0L, DataUtils.getLong("five", 0L));
     }
 
     @Test
-    void getBoolean() {
+    void getBooleanTest() {
         assertTrue(DataUtils.getBoolean("true", false));
         assertFalse(DataUtils.getBoolean("false", false));
         assertFalse(DataUtils.getBoolean("text", false));
+    }
+
+    @Test
+    void convertCodeToASCIITest() {
+        assertEquals(DataUtils.convertCodeToASCII(65), "A");
+        assertEquals(DataUtils.convertCodeToASCII(48), "0");
+        assertEquals(DataUtils.convertCodeToASCII(13), "ENTER");
+        assertEquals(DataUtils.convertCodeToASCII(123), "F12");
+        assertEquals(DataUtils.convertCodeToASCII(1), "{1}");
+        assertEquals(DataUtils.convertCodeToASCII(192), "~");
+    }
+
+    @Test
+    void convertASCIIToCodeTest() {
+        assertEquals(DataUtils.convertASCIIToCode("A"), 65);
+        assertEquals(DataUtils.convertASCIIToCode("0"), 48);
+        assertEquals(DataUtils.convertASCIIToCode("ENTER"), 13);
+        assertEquals(DataUtils.convertASCIIToCode("F12"), 123);
+        assertEquals(DataUtils.convertASCIIToCode("{1}"), 1);
+        assertEquals(DataUtils.convertASCIIToCode("{200}"), 200);
+        assertEquals(DataUtils.convertASCIIToCode("~"), 192);
     }
 }
